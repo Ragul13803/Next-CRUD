a8b827fe3f8d2c5d59cd92743aa660d2e7fdd4a8
Ragul-HEPL <ragul.s@hepl.com>
2025-02-06 13:07:52 +0530

    Project for Deployment

--- app/api/getCustomers/route.ts
+++ app/api/getCustomers/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { connectToDatabase } from '../../../lib/mongodb';
 
 // GET /api/users - Fetch all users from the database
-export async function GET() { 
+export async function GET() {
   try {
     const db = await connectToDatabase();
     const customers = await db.collection('customers').find().toArray();
